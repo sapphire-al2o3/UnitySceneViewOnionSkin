a0cfd297a70d3f0f5dc98c382d2e3d800c90fd01
sapphire-al2o3 <t_telkun@hotmail.com>
2022-10-06 19:29:31 +0900

    ブレンド率のスライダーを付ける

--- Assets/Editor/UnitySceneViewOnionSkin.cs
+++ Assets/Editor/UnitySceneViewOnionSkin.cs
@@ -10,6 +10,7 @@ public static class UnitySceneViewOnionSkin
     }
 
     static RenderTexture renderTexture = null;
+    static float alpha = 0.5f;
 
     static void Capture(Camera camera)
     {
@@ -32,10 +33,12 @@ public static class UnitySceneViewOnionSkin
 
         if (renderTexture != null)
         {
-            GUI.color = new Color(1, 1, 1, 0.5f);
+            GUI.color = new Color(1, 1, 1, alpha);
             GUI.DrawTexture(new Rect(0, 1, renderTexture.width, renderTexture.height), renderTexture);
             GUI.color = color;
         }
+
+        alpha = EditorGUILayout.Slider(alpha, 0.0f, 1.0f, GUILayout.Width(120));
 
         if (GUILayout.Button("Capture", GUILayout.Width(120)))
         {
